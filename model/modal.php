ad61aad2d131b0b1135ea984be79fa44d4355ac6
oladayo ahmod <oladayoahmod112@gmail.com>
2021-01-20 12:07:25 -0800

    Updated the signup
    updated the signup functionality to have the image functionality for upload

--- model/modal.php
+++ model/modal.php
@@ -45,7 +45,7 @@
             echo $error;
         }
         // signup form method
-        public function signup($name,$email,$username,$password,$c_password,$number){
+        public function signup($name,$email,$username,$password,$c_password,$number,$image){
             $error = '';
             error_reporting(E_ALL);
 
@@ -61,10 +61,16 @@
                 else if ($password !== $c_password) {
                     $error = '<div class="alert alert-danger" role="alert">Passwords do not match! </div>';
                 }
+                else if (!preg_match("!image!",$_FILES['image']['type'])) {
+                    $error = '<div class="alert alert-danger" role="alert">Error! not an image </div>';
+                }
                 else{
-                    $query = "INSERT INTO users(`fullname`,username,email,`password`,phone_num) VALUES(?,?,?,?,?)";
+                    $image_exp = explode('.',$_FILES['image']['name']);
+                    $image_path = "images/". round(microtime(true)) . '.' .strtolower(end($image_exp));
+                    move_uploaded_file($_FILES['image']['tmp_name'],$image_path);
+                    $query = "INSERT INTO users(`fullname`,username,email,`password`,phone_num,image_dir) VALUES(?,?,?,?,?,?)";
                     $stmt = $this->conn->prepare($query);
-                    $stmt->bind_param('ssssi',$name,$username,$email,$password,$number);
+                    $stmt->bind_param('ssssis',$name,$username,$email,$password,$number,$image_path);
                     if ($stmt->execute()){
                         // // select id of the inserted  user
                         $user_id = mysqli_insert_id($this->conn);
@@ -144,6 +150,42 @@
             echo $error;
         }
 
+        // profile image functionality
+        public function update_img($id,$image){
+            // setting error message
+            $error = '';
+            if ($_SERVER['REQUEST_METHOD'] == 'POST') {// if the request method is post
+                if (empty($image)) {
+                    $error = '<div class="alert alert-danger" role="alert"> No image selected! </div>';
+                }
+                else if (!preg_match("!image!",$_FILES['image']['type'])) {
+                    $error = '<div class="alert alert-danger" role="alert">Error! Not image </div>';
+                }
+                else {
+                    $image_exp = explode('.',$_FILES['image']['name']);
+                    $image_path = "../images/". round(microtime(true)) . '.' .strtolower(end($image_exp));
+                    if (move_uploaded_file($_FILES['image']['tmp_name'],$image_path)){// if the image is moved to the folder
+                        // update the image path in the database
+                        $update = "UPDATE users SET image_dir = ? WHERE id = ? LIMIT 1";
+                        $stmt = $this->conn->prepare($update);
+                        $stmt->bind_param('si',$image_path,$id);
+                        if ($stmt->execute()) {
+                            $error = '<div class="alert alert-success" role="alert">Image uploaded successfully! </div>';
+                        }
+                        else {
+                            $error = '<div class="alert alert-danger" role="alert">Error updating image! </div>';
+                        }   
+                    }
+                    else{
+                        $error = '<div class="alert alert-danger" role="alert">Error! Not image </div>'. mysqli_error($this->conn);
+
+                    } 
+                   
+                }
+                // echo $error . mysqli_error($this->conn);
+            } 
+            echo $error;
+            }
 
     }
 ?>
--- signup.php
+++ signup.php
@@ -23,24 +23,27 @@
                         $password = md5($_POST['password']);
                         $c_password = md5($_POST['c_password']);
                         $number = strip_tags($_POST['number']);
+                        $image = $_FILES['image'];
                         $model = new Model;
-                        $model->signup($name,$email,$username,$password,$c_password,$number);
+                        $model->signup($name,$email,$username,$password,$c_password,$number,$image);
                     }
 
                 ?>
                     <form class="form-group" enctype="multipart/form-data" method="POST" action="">
                         <label for="name">Name</label>
-                        <input type="text" name="name" placeholder="" required class="form-control"><br>
+                        <input type="text" name="name" required placeholder="" required class="form-control"><br>
                         <label for="email">Email</label>
-                        <input type="email" name="email" class="form-control" id=""><br>
+                        <input type="email" name="email" required class="form-control" id=""><br>
                         <label for="username">Username</label>
-                        <input type="text" name="username" class="form-control" id=""><br>
+                        <input type="text" name="username" required class="form-control" id=""><br>
                         <label for="username">Phone number</label>
-                        <input type="number" name="number" class="form-control" id=""><br>
+                        <input type="number" name="number" required class="form-control" id=""><br>
                         <label for="password">Password</label>
-                        <input type="password" name="password" class="form-control"><br>
+                        <input type="password" name="password" required class="form-control"><br>
                         <label for="password">Confirm password</label>
-                        <input type="password" name="c_password" class="form-control"><br>
+                        <input type="password" name="c_password" required class="form-control"><br>
+                        <label for="picture">Choose image</label>
+                        <input type="file" name="image" required id=""><br><br>
                         <div style="display:flex;">
                             <button type="submit" class="btn btn-primary mr-auto" name="signup">Submit</button>
                             <a class="btn btn-danger" href="index.php">Login</a>
